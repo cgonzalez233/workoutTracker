6a611edb0533306c5073ee14558efb6af43e6487
Christian Gonzalez <cgonzalez233@yahoo.com>
2021-01-22 14:49:03 -0500

    finished app v1

--- models/Workout.js
+++ models/Workout.js
@@ -2,22 +2,35 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-  day: Date,
-  exercises: [{
-    type: String,
-    name: String,
-    duration: Number,
-    weight: Number,
-    reps: Number,
-    sets: Number,
-    distance: Number
-  }]
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      duration: {
+        type: Number,
+        required: true,
+      },
+      weight: Number,
+      reps: Number,
+      sets: Number,
+      distance: Number,
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
 module.exports = Workout;
-
 
 // {
 //   day: new Date().setDate(new Date().getDate()-10),
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const compression = require("compression");
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,40 +14,65 @@ app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(compression());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+});
+
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
 
 app.get("/api/workouts", (req, res) => {
   db.Workout.find({})
-    .then(dbWorkout => {
+    .then((dbWorkout) => {
       res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
 });
 
-app.put("/api/workouts/:id", ({ body , params}, res) => {
-  db.Workout.findByIdAndUpdate(params.id, {$push: { exercises: body }})
-    .then(dbWorkout => {
+app.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkout) => {
+      console.log(dbWorkout);
       res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
 });
 
-app.post("/api/workouts", ({body}, res) => {
+app.put("/api/workouts/:id", ({ body, params }, res) => {
+  console.log(body, params);
+  db.Workout.findByIdAndUpdate(params.id, { $push: { exercises: body } })
+    .then((dbWorkout) => {
+      console.log(dbWorkout, "test");
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json(err);
+    });
+});
+
+app.post("/api/workouts", ({ body }, res) => {
   db.Workout.create(body)
-    .then(dbWorkout => {
+    .then((dbWorkout) => {
       res.json(dbWorkout);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
-})
+});
 
 // Start the server
 app.listen(PORT, () => {
